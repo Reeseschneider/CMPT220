353161a3e71ba9e656db31e9dfa33071c6a14268
Reeseschneider <ris0120@icloud.com>
2026-03-29 14:42:35 -0400

    Create debug5.java

---
+++ debug5.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+
+/*
+Referencing your homework from https://github.com/melissachodziutko/CMPT-220/blob/main/Assignment%203/Debug.java
+Can you take problems 2, 3, and 5 and turn them into methods, and then call those methods in main?
+Things that may trip you up while working on this:
+1. the code works the same whether it's in a method or not, it just needs some minor variable adjustments probably
+2. be wary of opening and closing brackets!! 
+3. methods can't go inside methods!
+4. methods go outside public static main, but inside public class main!
+5. do you need to return the value? if so, make sure it isn't void
+
+I give you two fully working method examples with and without parameters and called them for your reference :)
+*/
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter your first number to be added");
+    int firstInput = sc.nextInt();
+    System.out.println("Enter your second number to be added");
+    int secondInput = sc.nextInt();
+    //remember this method is returning, so we need to print it out
+    System.out.println(add(firstInput,secondInput));
+
+    System.out.println("im going to print out the word Dog now.");
+    printDog();
+
+    System.out.println("Enter a number and I will tell you the factorial");
+    Integer num1 = sc.nextInt();
+    System.out.println(factorialnum(num1));
+      
+    }
+
+  
+    public static int add(int zyx, int tuv){
+        return(zyx + tuv);
+    }
+
+    public static void printDog(){
+        System.out.println("Dog");
+    }
+    public static int factorialnum(int x) {
+        int factorial = 1;
+        for (int i = 1; i <=x; i++){
+            factorial = factorial * i;
+        }
+            return(factorial);
+        
+    }
+
+  
+}
